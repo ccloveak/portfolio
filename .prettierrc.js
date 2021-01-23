bacf44d3dfc07dc44e15d9aa5c4831e87692db0d
ccloveak <superstarzsw@gmail.com>
2021-01-24 02:20:38 +0900

    feat: 基本配置

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "tabWidth": 4,
+    "useTabs": false,
+    "singleQuote": true,
+    "trailingComma": "all",
+    "printWidth": 100,
+    "endOfLine": "auto",
+    "bracketSpacing": true
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # portfolio
+
 这是一个普普通通的作品集。
+
+## 背景
+
+-   对象是艺术生，需要一个展示自己平台。
+-   顺便练习下 react hooks
+
+## ui 的选择
+
+-   [x] material
+-   [] ant design 感觉有点商务
