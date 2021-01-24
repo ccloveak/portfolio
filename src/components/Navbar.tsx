44d6cd304457b6c3b4d4f0734def89f85849f457
ccloveak <superstarzsw@gmail.com>
2021-01-24 13:00:54 +0900

    feat: 导入css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,23 +11,39 @@ import {
     Typography,
     Box,
 } from '@material-ui/core';
-
+import { makeStyles } from '@material-ui/core/styles';
 import { ArrowBack, AssignmentInd, Home, Apps, ContactMail } from '@material-ui/icons';
+import avatar from '../avatar.jpeg';
+
+//调整css样式
+const useStyles = makeStyles({
+    menuSliderContainer: {
+        width: 250,
+        background: '#511',
+        height: '30rem',
+    },
+});
 
 const Navbar = () => {
+    const classes = useStyles();
     return (
-        <Box component="nav">
-            <AppBar position="static" style={{ background: '#222' }}>
-                <Toolbar>
-                    <IconButton>
-                        <ArrowBack style={{ background: 'tomato' }} />
-                    </IconButton>
-                    <Typography variant="h5" style={{ background: 'ten' }}>
-                        画画
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </Box>
+        <>
+            <Box className={classes.menuSliderContainer} component="div">
+                <Avatar src={avatar} alt="yy的头像" />
+            </Box>
+            <Box component="nav">
+                <AppBar position="static" style={{ background: '#222' }}>
+                    <Toolbar>
+                        <IconButton>
+                            <ArrowBack style={{ background: 'tomato' }} />
+                        </IconButton>
+                        <Typography variant="h5" style={{ background: 'ten' }}>
+                            画画
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </>
     );
 };
 export default Navbar;
